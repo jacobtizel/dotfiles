73ac91aa55f6f8bc3e9cd4ebb1a4a14e4e6facaf
Jacob Tizel <jacob.tizel@gmail.com>
2024-04-05 12:56:57 -0700

    Added fish config

---
+++ fish/conf.d/abbreviations.fish
@@ -0,0 +1,27 @@
+abbr cosc "cd /mnt/c/Users/tizre/Documents/UBC/COSC\ stuff/"
+abbr moosft "fortune -n 200 -s | cowsay -f moose | lolcat"
+abbr moosfort "fortune -n 200 -l | cowsay -f moose | lolcat"
+
+abbr gco "git checkout"
+
+# do not delete / or prompt if deleting more than 3 files at a time
+abbr rm "rm -I --preserve-root"
+ 
+# confirmation
+abbr mv "mv -i"
+abbr cp "cp -i"
+abbr ln "ln -i"
+
+abbr lsd "ls -l --color | grep --color=never '^d'"
+abbr la "ls -la --color"
+abbr l "ls -l --color"
+abbr c "clear"
+abbr .. "cd .."
+abbr ... "cd ../.."
+abbr .... "cd ../../.."
+abbr ..... "cd ../../../.."
+abbr g "git"
+
+
+abbr -a --position anywhere tz "/mnt/c/Users/tizre/"
+abbr -a --position anywhere firefoxwsl "'/mnt/c/Program Files/Mozilla Firefox/firefox.exe'"
---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+abbr -a -- cosc "cd /mnt/c/Users/tizre/Documents/UBC/COSC\ stuff/"
+abbr -a -- moosft "fortune -n 200 -s | cowsay -f moose | lolcat"
+abbr -a -- moosfort "fortune -n 200 -l | cowsay -f moose | lolcat"
+set -gx EDITOR nvim 
+
+abbr -a -- gco git\ checkout
+
+# do not delete / or prompt if deleting more than 3 files at a time #
+abbr -a -- rm "rm -I --preserve-root"
+ 
+# confirmation #
+abbr -a -- mv "mv -i"
+abbr -a -- cp "cp -i"
+abbr -a -- ln "ln -i"
+
+abbr -a --position anywhere tz "/mnt/c/Users/tizre/"
+abbr -a --position anywhere firefoxwsl "'/mnt/c/Program Files/Mozilla Firefox/firefox.exe'"
+
+# set -gx BROWSER 'explorer.exe'
+set -gx BROWSER "powershell.exe /C start"
+
+if status --is-interactive
+    fortune -n 200 -s | cowsay -f moose | lolcat
+end
